b07c27ee51c73657f4a5149c1373e06ac277925d
hassan <hk5708102@gmail.com>
2023-08-16 18:27:01 +0300

    add custom app bar to home view

--- lib/Features/home/data/presentation/views/home_view.dart
+++ lib/Features/home/data/presentation/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/Features/home/data/presentation/views/widgets/home_view_body.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -7,7 +8,7 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-
+       body: HomeViewBody(),
     );
   }
 }
