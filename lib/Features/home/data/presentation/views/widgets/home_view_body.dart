8e92f99be4bc53f0a7759eca50270fd14128cde5
hassan <hk5708102@gmail.com>
2023-08-17 19:13:36 +0300

    add Feature list view item to home

--- lib/Features/home/data/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/data/presentation/views/widgets/home_view_body.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'custom_app_bar.dart';
-import 'custom_list_view_item.dart';
+import 'feature_list_view_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({Key? key}) : super(key: key);
