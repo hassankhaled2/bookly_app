86c7d9e216964465ceceb72ce402493f26760364
hassan <hk5708102@gmail.com>
2023-08-20 19:07:54 +0300

    create book details

---
+++ lib/Features/home/data/presentation/views/book_details_view.dart
@@ -0,0 +1,14 @@
+
+import 'package:bookly_app/Features/home/data/presentation/views/widgets/book_deatils_view_body.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsViews extends StatelessWidget {
+  const BookDetailsViews({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BookDetailsViewBody(),
+    );
+  }
+}
