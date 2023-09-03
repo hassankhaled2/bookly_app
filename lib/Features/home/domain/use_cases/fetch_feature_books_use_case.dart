972e8b8b9f988044eaeb667e58a01abe80ddfcea
hassan <hk5708102@gmail.com>
2023-09-03 17:56:36 +0300

    refactor to use case class

--- lib/Features/home/domain/use_cases/fetch_feature_books_use_case.dart
+++ lib/Features/home/domain/use_cases/fetch_feature_books_use_case.dart
@@ -2,19 +2,28 @@ import 'package:bookly_app/Features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 import '../../../../core/errors/failures.dart';
+import '../../../../core/use_cases/use_cases.dart';
 import '../entites/book_entity.dart';
 
-class FetchFeatureBooksUseCase
+class FetchFeatureBooksUseCase extends UseCase<List<BookEntity>,NoParam>
 {
   final HomeRepo homeRepo;
 
   FetchFeatureBooksUseCase(this.homeRepo);
-  Future<Either<Failure,List<BookEntity>>>fetchFeaturedBooks()
-  {
-  //  هنا ال use case
-    // check permission
-  return homeRepo.fetchFeaturedBooks();
-  //انت هنا مجرد علمت method فقط  علشان تستدعى ال method اللى فى ال Home repo
-  // ممكن هتضيف حاجه مستقبلا هنا علشان كده مهمه
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam?param]) async {
+
+    return await homeRepo.fetchFeaturedBooks();
   }
+  // FetchFeatureBooksUseCase(this.homeRepo);
+  // Future<Either<Failure,List<BookEntity>>> call()
+  // {
+  //   //call or execute
+  // //  هنا ال use case
+  //   // check permission
+  // return homeRepo.fetchFeaturedBooks();
+  // //انت هنا مجرد علمت method فقط  علشان تستدعى ال method اللى فى ال Home repo
+  // // ممكن تضيف حاجه مستقبلا هنا علشان كده مهمه
+  // }
 }
